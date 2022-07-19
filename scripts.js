264c54a7a487d61a5fa239eca89d3b91dde38c64
kMose <bennegron62@gmail.com>
2022-07-19 14:54:34 -0400

    Implement createSketchBox(size) which is called on start to create drawpad

--- scripts.js
+++ scripts.js
@@ -1,26 +1,7 @@
 
+createSketchBox(100);
 
-let container = document.querySelector(".container");
-let newLine = document.createElement("div");
 
-    
-for (let i = 0; i < 100; i++){
-    // Creates a container to hold a line of boxes.
-    newLine = document.createElement("div");
-    newLine.classList.add("line");
-    container.appendChild(newLine);
-    
-    for(let j = 0; j < 100; j++){
-         // Creates the divs that are appended to the newLine div.
-        box = document.createElement("div");
-        box.classList.add("box")
-        newLine.appendChild(box);
-    }
-}
-
-boxes = document.querySelectorAll(".box");
-
-boxes.forEach(box => box.addEventListener("mouseover", () => boxTouched(box)));
 
 
 function boxTouched(boxElement){
@@ -28,10 +9,33 @@ function boxTouched(boxElement){
 }
 
 function clearSketchBox(){
-    let element = document.querySelector(".container");
+    let element = document.querySelector(".sketch-box");
     element.remove();
 }
 
-function createSketchBox(){
+function createSketchBox(size){
     
+    let container = document.querySelector(".container");
+
+    let sketchBox = document.createElement("div");
+    sketchBox.classList.add("sketch-box");
+    container.appendChild(sketchBox);
+    let newLine = document.createElement("div");
+
+    for (let i = 0; i < size; i++){
+        // Creates a container to hold a line of boxes.
+        newLine = document.createElement("div");
+        newLine.classList.add("line");
+        sketchBox.appendChild(newLine);
+        
+        for(let j = 0; j < size; j++){
+            // Creates the divs that are appended to the newLine div.
+            box = document.createElement("div");
+            box.classList.add("box")
+            newLine.appendChild(box);
+        }
+    }
+
+    boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.addEventListener("mouseover", () => boxTouched(box)));
 }
