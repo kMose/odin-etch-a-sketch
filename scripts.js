b9c114d8365d80840dcd13a68fb0b0e6d6cc6b3e
kMose <bennegron62@gmail.com>
2022-07-19 14:34:30 -0400

    add border for container

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ let container = document.querySelector(".container");
 let newLine = document.createElement("div");
 
     
-for (let i = 0; i < 32; i++){
+for (let i = 0; i < 128; i++){
     // Creates a container to hold a line of boxes.
     newLine = document.createElement("div");
     newLine.classList.add("line");
     container.appendChild(newLine);
     
-    for(let j = 0; j < 32; j++){
+    for(let j = 0; j < 128; j++){
          // Creates the divs that are appended to the newLine div.
         box = document.createElement("div");
         box.classList.add("box")
